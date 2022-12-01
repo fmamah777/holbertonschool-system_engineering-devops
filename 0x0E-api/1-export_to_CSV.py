b24a1e0345a487a37add5c40189b7fd7b2d295e3
fmamah777 <fm.mamah@gmail.com>
2022-11-30 20:15:43 -0800

    correctiom for task 2 trial

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -1,42 +1,29 @@
 #!/usr/bin/python3
-"""
-This example will take the employees id number 
-and return the amount of tasks done vs the total
-amount of tasks also listing what taks are done then it saves the
-task information for that user into a .csv file.
-"""
+""" REST API to request information"""
 import csv
+from os import sys
 import requests
-import sys
 
 
-if __name__ == '__main__':
-    # check for valid input, argument must be type int #
-    if sys.argv[1].isdigit():
-
-        # assign values to variables #
+if __name__ == "__main__":
+    try:
         user_id = sys.argv[1]
-        employee = requests.get(
-            "https://jsonplaceholder.typicode.com/users/{}"
-            .format(user_id)).json()
-        to_do = requests.get(
-            "https://jsonplaceholder.typicode.com/todos/?userId={}"
-            .format(user_id)).json()
-        completed = []
-        for done in to_do:
-            if done.get("completed") is True:
-                completed.append(done.get("title"))
+        id = int(user_id)
 
-        # requested outputs #
-        print("Employee {} is done with tasks({}/{}):".format(
-            employee.get("name"), len(completed), len(to_do)))
-        for title in completed:
-            print("\t {}".format(title))
+    except ValueError:
+        print("ID not INT")
 
-        # request from values to CSV format file #
-        with open("{}.csv".format(user_id), 'w') as file:
-            csv_write = csv.writer(file, quoting=csv.QUOTE_ALL)
-            for item in to_do:
-                csv_write.writerow([
-                    user_id, employee.get("username"),
-                    item.get("completed"), item.get("title")])
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(id))
+    name = user.json()["name"]
+    username = user.json()["username"]
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+    file = user_id + ".csv"
+
+    with open(file, 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for k in todos.json():
+            if k['userId'] == id:
+                data = [k['userId'], username, k['completed'], k['title']]
+                writer.writerow(data)
