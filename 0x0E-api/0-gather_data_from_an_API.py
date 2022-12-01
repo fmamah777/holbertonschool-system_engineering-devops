cc84b0096d15c8a648a0a30e6433fd86ed4c5aa7
fmamah777 <fm.mamah@gmail.com>
2022-11-30 20:11:43 -0800

    corrections updated

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
-"""
-This example will take the employees id number
-and return the amount of tasks done vs the total
-amount of tasks also listing what taks are done.
-"""
-
+"""Uses a REST API to retrieve information,
+for a given employee ID, returns information about
+his/her TODO list progress."""
+from os import sys
 import requests
-import sys
 
 
-if __name__ == '__main__':
-    # check for valid input, argument passed must be type int #
-    if sys.argv[1].isdigit():
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    id = int(user_id)
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(id))
+    name = user.json().get("name")
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
 
-        # assign values to variables #
-        user_id = sys.argv[1]
-        employee = requests.get(
-            "https://jsonplaceholder.typicode.com/users/{}"
-            .format(user_id)).json()
-        to_do = requests.get(
-            "https://jsonplaceholder.typicode.com/todos/?userId={}"
-            .format(user_id)).json()
-        completed = []
-        for done in to_do:
-            if done.get("completed") is True:
-                completed.append(done.get("title"))
+    all = 0
+    complete = 0
 
-        # print requested outputs #
-        print("Employee {} is done with tasks({}/{}):".format(
-            employee.get("name"), len(completed), len(to_do)))
-        for title in completed:
-            print("\t {}".format(title))
+    for k in todos.json():
+        if k.get('userId') is id:
+            all += 1
+        if k.get('userId') is id and k.get('completed') is True:
+            complete += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(name, complete, all))
+
+    for k in todos.json():
+        if k.get('userId') is id and k.get('completed') is True:
+            print("\t {}".format(k.get('title')))
